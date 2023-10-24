be93b77f7d2dd5344bf8741f42eca6ca2fddc734
Aida <aida2001@mail.ru>
2023-10-25 00:19:14 +0600

    deleted creamdiplom

--- CreamDiplom/dev/config.ts
+++
@@ -1,3 +0,0 @@
-//здесь прописываем константы
-export const MONGO = 'mongodb://127.0.0.1:27017/shopCream?directConnection=true&serverSelectionTimeoutMS=2000'
-export const PORT = process.env.PORT || 7000
